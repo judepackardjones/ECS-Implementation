0e45cba5f55563565ec979daac412347ac8ac2ec
Jude Packard-Jones <judepackardjones@gmail.com>
2025-12-09 11:02:33 -0500

    Added Player Component

--- src/ecs/components.rs
+++ src/ecs/components.rs
@@ -6,10 +6,15 @@ pub struct Position {
     pub x: i32,
     pub y: i32,
 }
-
+#[derive(Debug, PartialEq)]
 pub struct Size {
     pub r: u32, 
 }
 
+#[derive(Debug, PartialEq)]
+pub struct Player {}
+
+
 impl Component for Position {}
 impl Component for Size {}
+impl Component for Player {}
